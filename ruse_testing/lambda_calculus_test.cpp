e9e1c0054ac8648627399f94fe5d12a7def110d4
Samuel B. Johnson <sabjohnso@yahoo.com>
2022-08-11 06:49:56 -0500

    Removed content of lambda_calculus and added an empty test for it

---
+++ ruse_testing/lambda_calculus_test.cpp
@@ -0,0 +1,14 @@
+//
+// ... Testing header files
+//
+#include <gtest/gtest.h>
+#include <ruse_testing/test_macros.hpp>
+
+//
+// ... ruse header files
+//
+#include <ruse/experimental/lambda_calculus.hpp>
+
+namespace ruse::experimental::testing {
+  TEST(variable, concept_affirmative) {}
+} // end of namespace ruse::experimental::testing
